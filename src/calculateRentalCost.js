0ade3ce6ceebaeaf273766d09c0e8000d10ed02c
PavloMolytovnyk <pavlomolytovny@icloud.com>
2026-04-03 19:30:12 +0300

    Change4

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,15 +7,15 @@ function calculateRentalCost(days) {
   const payment = 40;
   const discontPerThirdDay = 20;
   const discontPerFifthDay = 50;
-  const longDay =7;
-  const shortDay =3;
+  const longDay = 7;
+  const shortDay = 3;
 
   if (days >= longDay) {
-    return days * payment - discontPerThirdDay;
+    return days * payment - discontPerFifthDay;
   }
 
   if (days >= shortDay) {
-    return days * payment - discontPerFifthDay;
+    return days * payment - discontPerThirdDay;
   }
 
   return days * payment;
