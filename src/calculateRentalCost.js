b8deb21e31b030827ec6f6539f62c9681b16096e
PavloMolytovnyk <pavlomolytovny@icloud.com>
2026-04-03 19:19:52 +0300

    Change

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  if (days <= 0) {
+    return 'Enter valid days';
+  }
+
+  if (days < 3) {
+    return days * 40;
+  }
+
+  if (days >= 3 && days < 7) {
+    return days * 40 - 20;
+  }
+
+  if (days >= 7) {
+    return days * 40 - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
